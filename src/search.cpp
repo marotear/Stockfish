5550720ea30bc065e94f4fb13d665f7f29173d62
marotear <marotear00@gmail.com>
2017-10-06 19:09:42 -0700

    different offset for PvNode in LMR

--- src/search.cpp
+++ src/search.cpp
@@ -981,7 +981,7 @@ moves_loop: // When in check search starts from here
                              + (*contHist[0])[movedPiece][to_sq(move)]
                              + (*contHist[1])[movedPiece][to_sq(move)]
                              + (*contHist[3])[movedPiece][to_sq(move)]
-                             - 4000;
+                             - (PvNode ? 3750 : 4200);
 
               // Decrease/increase reduction by comparing opponent's stat score
               if (ss->statScore >= 0 && (ss-1)->statScore < 0)
