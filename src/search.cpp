6c32d0f63be8d4d7702f03b2c16f85abe857db64
marotear <marotear@users.noreply.github.com>
2018-03-17 15:22:22 -0700

    Extra bonus for new quiet ttMove when updating stats

--- src/search.cpp
+++ src/search.cpp
@@ -1122,7 +1122,7 @@ moves_loop: // When in check, search starts from here
     {
         // Quiet best move: update move sorting heuristics
         if (!pos.capture_or_promotion(bestMove))
-            update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(depth));
+            update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount, stat_bonus(ttMove && bestMove != ttMove ? depth + ONE_PLY : depth));
         else
             update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth));
 
