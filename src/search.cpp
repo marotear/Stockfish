7d3eadbd3596f6f970be04764d3f740941fdaea0
marotear <marotear@users.noreply.github.com>
2018-06-10 10:23:42 -0700

    More pruning for cutNode with positive previous statScore

--- src/search.cpp
+++ src/search.cpp
@@ -941,7 +941,8 @@ moves_loop: // When in check, search starts from here
               }
 
               // Reduced depth of the next LMR search
-              int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount), DEPTH_ZERO) / ONE_PLY;
+              int lmrDepth = std::max(newDepth - reduction<PvNode>(improving, depth, moveCount)
+                                               - (cutNode && (ss-1)->statScore >= 0) * ONE_PLY, DEPTH_ZERO) / ONE_PLY;
 
               // Countermoves based pruning (~20 Elo)
               if (   lmrDepth < 3
