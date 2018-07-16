35d50a64974f32f307e90a61bda39a21a0d32b12
marotear <marotear@users.noreply.github.com>
2018-07-15 18:54:18 -0700

    Increase bonus by quietsCnt

--- src/search.cpp
+++ src/search.cpp
@@ -1476,7 +1476,7 @@ moves_loop: // When in check, search starts from here
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     thisThread->mainHistory[us][from_to(move)] << bonus;
-    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus);
+    update_continuation_histories(ss, pos.moved_piece(move), to_sq(move), bonus + quietsCnt);
 
     if (is_ok((ss-1)->currentMove))
     {
